975410b23d760b19b20c71a492ef0ae6e3a58ef4
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2022-08-02 22:23:20 -0500

    Integration test for findById case on UserContorller

---
+++ src/test/java/training/path/academicrecordsystem/controllers/UserControllerTests.java
@@ -0,0 +1,61 @@
+package training.path.academicrecordsystem.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import training.path.academicrecordsystem.model.User;
+import training.path.academicrecordsystem.services.IUserService;
+
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+public class UserControllerTests {
+
+    @Autowired
+    WebApplicationContext context;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    IUserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        this.mockMvc = MockMvcBuilders
+                .webAppContextSetup(this.context)
+                .build();
+    }
+
+    @Test
+    void givenValidId_whenFindUserById_thenReturnStatusCodeOkAndUser() throws Exception {
+        long userId = 1L;
+        User user = new User();
+        user.setName("Juan");
+        given(userService.findById(anyLong())).willReturn(user);
+
+        ResultActions response = mockMvc.perform(get("/users/{id}", userId));
+
+        response.andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.name", is(user.getName())));
+    }
+
+}
