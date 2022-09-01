ccf5f633453ae34711b9465a8ec658c0f144ce66
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2022-09-01 10:13:45 -0500

    Changes in CareerController tests

--- src/test/java/training/path/academicrecordsystem/controllers/CareerControllerTests.java
+++ src/test/java/training/path/academicrecordsystem/controllers/CareerControllerTests.java
@@ -1,43 +1,53 @@
 package training.path.academicrecordsystem.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import training.path.academicrecordsystem.controllers.dtos.CareerDTO;
 import training.path.academicrecordsystem.model.Career;
 import training.path.academicrecordsystem.repositories.implementations.CareerRepository;
+import training.path.academicrecordsystem.security.jwtauth.JWTTokenGeneratorService;
 import training.path.academicrecordsystem.services.implementations.CareerService;
 
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 @Disabled("Disable for Spring Security")
 public class CareerControllerTests {
 
     @Autowired
     WebApplicationContext context;
 
-    @Autowired
-    MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
+
+    @Autowired
+    JWTTokenGeneratorService tokenGeneratorService;
 
     @MockBean
     CareerService careerService;
@@ -45,11 +55,25 @@ public class CareerControllerTests {
     @MockBean
     CareerRepository careerRepository;
 
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @Test
+    @WithUserDetails("admin1.test1")
     void givenCareerDTO_whenSave_thenItReturnsOk() throws Exception {
         CareerDTO careerDTO = CareerDTO.builder().name("System Engineering").build();
 
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String token = tokenGeneratorService.generateJWT((UserDetails) auth.getPrincipal());
+
         ResultActions response = mockMvc.perform(post("/careers")
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(careerDTO)));
 
