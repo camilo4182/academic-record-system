1278be60fd08f2e469b9725a5e72af6e1acda520
Johan Camilo Cortes Ocampo <camilo4182@gmail.com>
2022-09-02 17:51:59 -0500

    Fix the UserFactory, now it returns an Optional instead of null

--- src/main/java/training/path/academicrecordsystem/factory/UserFactory.java
+++ src/main/java/training/path/academicrecordsystem/factory/UserFactory.java
@@ -5,16 +5,18 @@ import training.path.academicrecordsystem.model.Professor;
 import training.path.academicrecordsystem.model.Student;
 import training.path.academicrecordsystem.model.User;
 
+import java.util.Optional;
+
 import static training.path.academicrecordsystem.security.interfaces.IRoles.*;
 
 public class UserFactory {
 
-    public static User create(String rol) {
+    public static Optional<User> create(String rol) {
         return switch (rol) {
-            case "ROLE_" + ADMIN -> new Administrator();
-            case "ROLE_" + STUDENT -> new Student();
-            case "ROLE_" + PROFESSOR -> new Professor();
-            default -> null;
+            case "ROLE_" + ADMIN -> Optional.of(new Administrator());
+            case "ROLE_" + STUDENT -> Optional.of(new Student());
+            case "ROLE_" + PROFESSOR -> Optional.of(new Professor());
+            default -> Optional.empty();
         };
     }
 
--- src/main/java/training/path/academicrecordsystem/repositories/rowmappers/UserWithRolRowMapper.java
+++ src/main/java/training/path/academicrecordsystem/repositories/rowmappers/UserWithRolRowMapper.java
@@ -1,22 +1,28 @@
 package training.path.academicrecordsystem.repositories.rowmappers;
 
+import lombok.SneakyThrows;
 import org.springframework.jdbc.core.RowMapper;
+import training.path.academicrecordsystem.exceptions.ResourceNotFoundException;
 import training.path.academicrecordsystem.factory.UserFactory;
 import training.path.academicrecordsystem.model.Role;
 import training.path.academicrecordsystem.model.User;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Optional;
 import java.util.UUID;
 
 public class UserWithRolRowMapper implements RowMapper<User> {
 
+    @SneakyThrows
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
         Role role = new Role();
         role.setRoleName(rs.getString("role"));
 
-        User user = UserFactory.create(role.getRoleName());
+        Optional<User> userOptional = UserFactory.create(role.getRoleName());
+        if (userOptional.isEmpty()) throw new ResourceNotFoundException("Role was not found");
+        User user = userOptional.get();
         user.setId(rs.getObject("user_id", UUID.class).toString());
         user.setUserName(rs.getString("username"));
         user.setPassword(rs.getString("password"));
